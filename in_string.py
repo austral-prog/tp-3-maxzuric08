35d3951e88c2fa535edd8fe8871afafa36efbe56
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-04-05 11:39:43 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,14 @@
 def check_vowels():
+    result1='a' in str(input().lower())
+    result2='e' in str(input().lower())
+    result3='i' in str(input().lower())
+    result4='o' in str(input().lower())
+    result5='u' in str(input().lower())
+    print('Contiene a:',result1)
+    print('Contiene e:',result2)
+    print('Contiene i:',result3)
+    print('Contiene o:', result4)
+    print('Contiene u:',result5)
     # Código a implementar utilizando input.
 
 
